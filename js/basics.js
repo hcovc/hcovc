f7f9fef81fa20193a31822398493c8ea345cf60d
YourFriend Paul <git@yourfriendpaul.com>
2022-07-11 13:06:34 -0700

    Remove jQuery. ScrollToTop is too fast, but oh well. 	modified:   css/basic.css 	modified:   index.html 	modified:   js/basics.js

--- js/basics.js
+++ js/basics.js
@@ -2,7 +2,7 @@
  * The main code for HCoVC. The object space for the code is provided by
  *  hcovcSpace(), which must be defined before this is loaded.
  */
- /* global console, jQuery, document, window, hcovcSpace */
+ /* global console, document, window, hcovcSpace */
  /* jshint esversion: 6 */
 
 // Make other spaces first so they're safe to use here.
@@ -14,59 +14,54 @@
 
 
 /* Bits from the old site that are often parts of other packages that are unused. */
-(function(oRoot, $){
+(function(oRoot){
     'use strict';
 
-    // I never figured out exactly what "a" did, but making a JQ collection is
-    //  adequate.
-    function aModFromJp(qspec) {
-        // Returns an JQ collection.
-        return $(qspec);
-    }
+    const sttClickableId = 'back-to-top';
 
-    function _sttHandler(elem) {
-        // Stop the anchor from causing its action.
-        elem.preventDefault();
-        return aModFromJp('html, body').animate({
-            scrollTop: 0
-        }, 'slow'),
-        // No idea what this does or why. Was no ; but jshint whined.
-        !1;
-    }
+    // Inits for the handlers. Call each item to set it up.
+    let initFuncs = {
 
-    // Inits for the handlers.
-    let clickers = {
-
-        // Init the scroll to top button. The orig did preventDefault elsewhere.
         scrollToTop: function() {
-            aModFromJp('a#back-to-top').click(_sttHandler);
+            const butn = document.getElementById(sttClickableId);
+            if (butn !== null) {
+                butn.addEventListener('click', function(event) {
+                    window.scroll({
+                        top: 0,
+                        left: 0,
+                        behavior: 'smooth'
+                    });
+                });
+            }
         },
 
-        // Don't worry about commas.
+        // So I don't have to worry about commas.
         lintrap: false
     };
 
     // Holding space:
-    oRoot.handlerIniters = clickers;
+    oRoot.handlerIniters = initFuncs;
 
-})(hcovcSpace(), jQuery);
+})(hcovcSpace());
 // hcovcSpace.handlerIniters.scrollToTop()
+
 
 (function(oRoot, win, doc){
     'use strict';
+    // Strictly debug code.
     const bcls = '.hcovc-3word';
 
+    // Displays which image was loaded when load is complete. Was for srcset.
     function imageStatus(selectSpec=bcls) {
 
         win.addEventListener('load', () => {
 
-          let boxes = doc.querySelectorAll(selectSpec);
-          for (const box of boxes) {
-              let image = box.querySelector('img');
-
-              let newElem = doc.createElement('p');
-              newElem.innerHTML = `Image: <code>${image.currentSrc}</code>`;
-              box.appendChild(newElem);
+        let boxes = doc.querySelectorAll(selectSpec);
+        for (const box of boxes) {
+            let imageEl = box.querySelector('img');
+            let newElem = doc.createElement('p');
+            newElem.innerHTML = `Image: <code>${imageEl.currentSrc}</code>`;
+            box.appendChild(newElem);
           }
         });
     }
